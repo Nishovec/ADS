7ac5601b9a0301f3e31de54d5351fea7ee5028f3
Nishovec <shaymardanov_E@inbox.ru>
2023-04-09 21:29:05 +0600

    1

--- lab14/Main.java
+++ lab14/Main.java
@@ -29,4 +29,56 @@ public class Main
         else
             return num*findFactorial(num-1);
     }
+    public static int findFibonacci(int n){
+        if(n==1) {
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+        return  findFibonacci(n-1)+findFibonacci(n-2);
+    }
+    public static int findPow(int n,int i){
+        if(i==1) {
+            return n;
+        }
+        if(i==0){
+            return 1;
+        }
+        if(n==1) {
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+        return  n*findPow(n,i-1);
+    }
+    public static void reverseArray(int[] arr, int start, int end) {
+        if(start >= end) {
+            return;
+        }
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverseArray(arr, start+1, end-1);
+    }
+    public static boolean isDigits(String s){
+        if (s.length() == 0) {
+            return false;
+        } else if (s.length() == 1) {
+            return Character.isDigit(s.charAt(0));
+        } else {
+            return Character.isDigit(s.charAt(0)) && isDigits(s.substring(1));
+        }
+    }
+    int[] array = new int[n];
+          for (int i=0; i<n; i++) {
+    array[i] = scanner.nextInt();
+}
+    reverseArray(array, 0, n-1);
+        System.out.println("Reversed array:");
+        for(int i=0; i<n; i++) {
+    System.out.print(array[i] + " ");
+
+}
 }
